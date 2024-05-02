40da163fb6f8aeebf9b5b608d77e605d5e392964
zyadhany <zeyad.hany2003@gmail.com>
2024-05-03 02:49:32 +0300

    solved task

---
+++ api/v1/app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""app"""
+from flask import Flask, make_response, jsonify
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+
+
+app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def tear(self):
+    ''' closes storage    '''
+    storage.close()
+
+
+if __name__ == '__main__':
+    HBNB_API_HOST = '0.0.0.0'
+    if getenv("HBNB_API_HOST") is not None:
+        HBNB_API_HOST = getenv("HBNB_API_HOST")
+    HBNB_API_PORT = 5000
+    if getenv("HBNB_API_PORT") is not None:
+        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
