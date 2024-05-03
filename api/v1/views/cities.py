7f36a021f00343cb90406b0a5b43aa65ba2c87fc
zyadhany <zeyad.hany2003@gmail.com>
2024-05-03 05:25:10 +0300

    solved task

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -9,6 +9,7 @@ from datetime import datetime
 import uuid
 
 
+@app_views.route('/states/<state_id>/cities', methods=['GET'])
 @app_views.route('/states/<state_id>/cities/', methods=['GET'])
 def list_cities_of_state(state_id):
     '''get all cities'''
@@ -21,8 +22,8 @@ def list_cities_of_state(state_id):
     return jsonify(cities)
 
 
-
-@app_views.route('/cities/<city_id>', methods=['GET'])
+@app_views.route('/states/<state_id>/cities', methods=['POST'])
+@app_views.route('/states/<state_id>/cities/', methods=['POST'])
 def get_city(city_id):
     '''get id'''
     city = storage.get(City, city_id)
@@ -46,17 +47,17 @@ def delete_city(city_id):
 @app_views.route('/states/<state_id>/cities/', methods=['POST'])
 def create_city(state_id):
     '''Creates a City'''
-    
+
     if not request.get_json():
         abort(400, 'Not a JSON')
     if 'name' not in request.get_json():
         abort(400, 'Missing name')
-    
+
     state = storage.get(State, state_id)
 
     if state is None:
         abort(404)
-    
+
     city = City(name=request.json['name'], state_id=state_id)
     city.save()
     return jsonify(city.to_dict()), 201
